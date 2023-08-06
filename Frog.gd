b94bda82cb9930fcd600186d7e22412610f85793
heliosalesjr <heliosalesjr@gmail.com>
2023-08-06 17:37:50 -0300

    frog AI scripting

---
+++ Frog.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player
+var chase = false
+var SPEED = 50
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	if chase == true:
+		get_node("AnimatedSprite2D").play("Jump")
+		player = get_node("../../Player/Player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true
+		else:
+
+			get_node("AnimatedSprite2D").flip_h = false
+		velocity.x = direction.x * SPEED
+	
+	else:
+		get_node("AnimatedSprite2D").play("Idle")
+		velocity.x = 0
+	move_and_slide()
+
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		chase = true
+
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false
