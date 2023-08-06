d8ec0a43a7e240d007ab5cda6daff83b66407469
heliosalesjr <heliosalesjr@gmail.com>
2023-08-06 18:07:31 -0300

    hurt and kill the frog

--- Frog.gd
+++ Frog.gd
@@ -5,21 +5,26 @@ var player
 var chase = false
 var SPEED = 50
 
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	if chase == true:
-		get_node("AnimatedSprite2D").play("Jump")
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Jump")
 		player = get_node("../../Player/Player")
 		var direction = (player.position - self.position).normalized()
 		if direction.x > 0:
 			get_node("AnimatedSprite2D").flip_h = true
 		else:
-
+			
 			get_node("AnimatedSprite2D").flip_h = false
 		velocity.x = direction.x * SPEED
 	
 	else:
-		get_node("AnimatedSprite2D").play("Idle")
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Idle")
 		velocity.x = 0
 	move_and_slide()
 
@@ -30,3 +35,11 @@ func _on_player_detection_body_entered(body):
 func _on_player_detection_body_exited(body):
 	if body.name == "Player":
 		chase = false
+
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		chase = false
+		get_node("AnimatedSprite2D").play("Death")
+		await get_node("AnimatedSprite2D").animation_finished
+		self.queue_free()
