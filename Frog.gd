df016dd975221e3f0888aca044152e16eee59b5c
heliosalesjr <heliosalesjr@gmail.com>
2023-08-07 20:10:00 -0300

    saving and loading the game

--- Frog.gd
+++ Frog.gd
@@ -48,11 +48,12 @@ func _on_player_death_body_entered(body):
 
 func _on_player_collision_body_entered(body):
 	if body.name == "Player":
-		body.health -= 1
+		Game.playerHP -= 3
 		death()
 		
 func death():
-	
+	Game.Gold += 5
+	Utils.saveGame()
 	chase = false
 	get_node("AnimatedSprite2D").play("Death")
 	await get_node("AnimatedSprite2D").animation_finished
--- main.gd
+++ main.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 
-
+func _ready():
+	#Utils.saveGame()
+	Utils.loadGame()
+	
 func _on_quit_pressed():
 	get_tree().quit()
 
